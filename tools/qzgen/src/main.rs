95612793fb415fab81199c29d672aa132dd94aac
ichigo <dev.honda.ichigo@gmail.com>
2023-06-22 19:47:32 +0900

    tools: Update QZgen

--- tools/qzgen/src/main.rs
+++ tools/qzgen/src/main.rs
@@ -5,7 +5,9 @@ use std::io::Write;
 use clap::Parser;
 use chrono::Utc;
 use glob::glob;
-use markdown::{ self, Span, Block };
+use reqwest;
+use rand::seq::SliceRandom;
+use markdown_to_text;
 
 #[derive(Parser)]
 #[clap(author, version, about)]
@@ -31,10 +33,10 @@ struct Cli
 
     #[clap
     (
-        long="no-report",
+        long="report",
         default_value="false"
     )]
-    no_report: bool,
+    report: bool,
 
     #[clap
     (
@@ -51,17 +53,26 @@ fn main()
     let cli = Cli::parse();
     let note_path = PathBuf::from(&cli.note_path);
     let output_path = PathBuf::from(&cli.output);
+
     let checksheet = fs::read_to_string(cli.checksheet)
         .expect("failed to read checksheet");
-    let terms = parse_checksheet(&checksheet);
+    let mut terms = parse_checksheet(&checksheet, note_path.clone());
+    let mut rng = rand::thread_rng();
+    terms.shuffle(&mut rng);
 
-    if cli.no_report == false
+    //  Output report.
+    if cli.report
     {
-        report(output_path.clone(), note_path.clone());
+        report(output_path.clone(), note_path.clone(), &terms);
+    }
+
+    for term in terms
+    {
+        //println!("{} link: {} content: {}", term.name, term.link, term.content);
     }
 }
 
-//  Get file path
+//  Get the file path.
 fn get_file_path( mut output_path: PathBuf, file_prefix: &str ) -> PathBuf
 {
     output_path
@@ -69,9 +80,16 @@ fn get_file_path( mut output_path: PathBuf, file_prefix: &str ) -> PathBuf
         (
             "{}_{}.log",
             file_prefix,
-            Utc::now().format("%Y%m%d_%H%M%S")
+            Utc::now().format("%Y%m%d_%I%M%S")
         ));
     output_path
+}
+
+struct Term
+{
+    name: String,
+    link: String,
+    content: String,
 }
 
 //  Parse checksheet to terms.
@@ -79,7 +97,7 @@ fn get_file_path( mut output_path: PathBuf, file_prefix: &str ) -> PathBuf
 //  Header parts (like `# Heading`) are skipped. Formats like
 //  `- [ ] [term](link)` are allowed for terms. Other formats may cause
 //  problems.
-fn parse_checksheet( checksheet: &str ) -> Vec<(String, String)>
+fn parse_checksheet( checksheet: &str, filepath: PathBuf ) -> Vec<Term>
 {
     let mut counter = 0;
     let lines = checksheet.lines();
@@ -101,16 +119,44 @@ fn parse_checksheet( checksheet: &str ) -> Vec<(String, String)>
             continue;
         }
 
-        let term = items[1].to_string();
-        let link = items[0].replace(['(', ')'], "");
-        result.push((term, link));
+        let name = items[1].to_string();
+        let mut link = items[0].replace(['(', ')'], "");
+        let mut content = String::new();
+
+        //  Get content
+        if let Some((link_part, title)) = link.clone().split_once('#')
+        {
+            let link_path = filepath.join(Path::new(link_part));
+            link = link_path.to_str().unwrap().to_string();
+            if link_path.exists() && link_path.is_file()
+            {
+                let target = "# ".to_string() + &title.to_lowercase();
+                let input = fs::read_to_string(link_path).unwrap();
+                if let Some(start) = input.find(&target)
+                {
+                    content = input[(start + target.len())..].to_string();
+                    if let Some(end) = content.find("# ")
+                    {
+                        content = content[..(end - 2)].to_string();
+                    }
+                }
+            }
+        }
+
+        let term = Term
+        {
+            name,
+            link: link.to_string(),
+            content: markdown_to_text::convert(content.trim()),
+        };
+        result.push(term);
     }
 
     result
 }
 
 //  Output report.
-fn report( output_path: PathBuf, note_path: PathBuf )
+fn report( output_path: PathBuf, note_path: PathBuf, terms: &[Term] )
 {
     let report_file_path = get_file_path(output_path, "report");
     let mut report_file = File::create(report_file_path)
@@ -123,13 +169,87 @@ fn report( output_path: PathBuf, note_path: PathBuf )
         .map(|e| e.unwrap())
         .collect::<Vec<_>>();
 
+    //  Check for broken links
+    let link_regex = regex::Regex::new(r"\[.*?\]\(([^)\s]+)\)").unwrap();
     for file in files
     {
-        let content = fs::read_to_string(file).unwrap();
-        let tokens = markdown::tokenize(&content);
+        let content = fs::read_to_string(file.clone()).unwrap();
 
-        for token in tokens
+        for (n, line) in content.lines().enumerate()
         {
+            for capture in link_regex.captures_iter(line)
+            {
+                if let Some(link) = capture.get(1)
+                {
+                    let link = link.as_str();
+                    if link.starts_with('#')
+                    {
+                        continue;
+                    }
+
+                    if check_link(link, file.clone()) == false
+                    {
+                        let log = format!
+                        (
+                            "[{}:{}] link error: {}",
+                            file.as_os_str().to_str().unwrap(),
+                            n + 1,
+                            link
+                        );
+                        println!("{}", log);
+                        let _ = writeln!(report_file, "{}", log);
+                    }
+                }
+            }
         }
     }
+
+    //  Check if sentences can be a quiz.
+    for term in terms
+    {
+        let name = &term.name;
+        let content = &term.content;
+        if content.contains(name) == false
+        {
+           let log = format!
+            (
+                "the term '{}' is not contained in the '{}'",
+                name,
+                content,
+            );
+            println!("{}", log);
+            let _ = writeln!(report_file, "{}", log);
+        }
+    }
+}
+
+fn check_link( link: &str, filepath: PathBuf ) -> bool
+{
+    if link.starts_with("http")
+    {
+        let response = reqwest::blocking::get(link);
+        if let Ok(res) = response
+        {
+            return res.status().is_success();
+        }
+    }
+
+    let base_path = filepath.parent().unwrap();
+    if let Some((link, title)) = link.split_once('#')
+    {
+        let link_path = base_path.join(Path::new(link));
+        if link_path.exists() && link_path.is_file()
+        {
+            let content = fs::read_to_string(link_path).unwrap();
+            let target = "# ".to_string() + &title.to_lowercase();
+            return content.to_lowercase().contains(&target);
+        }
+    }
+    else
+    {
+        let link_path = base_path.join(Path::new(link));
+        return link_path.exists();
+    }
+
+    return false;
 }
