5eb76d8d60d702743f38f3b32f49dadd303e2d41
ichigo <dev.honda.ichigo@gmail.com>
2023-06-23 09:31:38 +0900

    tools: Update QZgen

--- tools/qzgen/src/main.rs
+++ tools/qzgen/src/main.rs
@@ -1,27 +1,29 @@
-use std::path::{ Path, PathBuf };
-use std::fs::{ self, File };
-use std::io::Write;
+/*
+
+    A CLI tool that automatically generates quizes from knowledge notes.
+
+*/
+
+use std::path::PathBuf;
+use std::fs;
 
 use clap::Parser;
-use chrono::Utc;
-use glob::glob;
-use reqwest;
-use rand::seq::SliceRandom;
-use markdown_to_text;
 
 #[derive(Parser)]
 #[clap(author, version, about)]
 struct Cli
 {
+    //  Path to the directory containing knowledge notes.
     #[clap
     (
         short='n',
-        long="note",
+        long="note-path",
         value_name="PATH",
         default_value="./note/ja"
     )]
     note_path: String,
 
+    //  Path to the checksheet.
     #[clap
     (
         short='c',
@@ -29,15 +31,9 @@ struct Cli
         value_name="PATH",
         default_value="./note/ja/checksheet.md"
     )]
-    checksheet: String,
+    checksheet_path: String,
 
-    #[clap
-    (
-        long="report",
-        default_value="false"
-    )]
-    report: bool,
-
+    //  Path to output the Learning log.
     #[clap
     (
         short='o',
@@ -45,211 +41,142 @@ struct Cli
         value_name="PATH",
         default_value="./"
     )]
-    output: String,
+    output_path: String,
 }
 
 fn main()
 {
     let cli = Cli::parse();
-    let note_path = PathBuf::from(&cli.note_path);
-    let output_path = PathBuf::from(&cli.output);
 
-    let checksheet = fs::read_to_string(cli.checksheet)
+    let checksheet = fs::read_to_string(cli.checksheet_path)
         .expect("failed to read checksheet");
-    let mut terms = parse_checksheet(&checksheet, note_path.clone());
-    let mut rng = rand::thread_rng();
-    terms.shuffle(&mut rng);
-
-    //  Output report.
-    if cli.report
-    {
-        report(output_path.clone(), note_path.clone(), &terms);
-    }
-
-    for term in terms
-    {
-        //println!("{} link: {} content: {}", term.name, term.link, term.content);
-    }
+    let terms = parse_checksheet(&checksheet);
 }
 
-//  Get the file path.
-fn get_file_path( mut output_path: PathBuf, file_prefix: &str ) -> PathBuf
-{
-    output_path
-        .push(format!
-        (
-            "{}_{}.log",
-            file_prefix,
-            Utc::now().format("%Y%m%d_%I%M%S")
-        ));
-    output_path
-}
 
+//------------------------------------------------------------------------------
+//  A structure representing a term.
+//------------------------------------------------------------------------------
+#[derive(Debug)]
 struct Term
 {
-    name: String,
+    //  Term.
+    term: String,
+
+    //  Path to the note.
     link: String,
+
+    //  Path to the fragment in the note.
+    flagment: String,
+
+    //  Explanation of the term.
     content: String,
 }
 
-//  Parse checksheet to terms.
+
+//------------------------------------------------------------------------------
+//  Parses the checksheet and convert it to a vector of terms.
 //
-//  Header parts (like `# Heading`) are skipped. Formats like
-//  `- [ ] [term](link)` are allowed for terms. Other formats may cause
-//  problems.
-fn parse_checksheet( checksheet: &str, filepath: PathBuf ) -> Vec<Term>
+//  The terms writen on the checksheet have the following format:
+//  - [term](link)
+//
+//  The link is the path to the note and the content is written in there.
+//------------------------------------------------------------------------------
+fn parse_checksheet( checksheet: &str ) -> Vec<Term>
 {
-    let mut counter = 0;
-    let lines = checksheet.lines();
-    lines.clone().for_each(|_| counter += 1);
-    let mut result = Vec::with_capacity(counter);
+    let mut terms = Vec::new();
 
-    for line in lines
+    for line in checksheet.lines()
     {
-        if line.len() <= 0 || line.starts_with('#')
+        if let Some(term) = parse_checksheet_line(line)
         {
-            continue;
+            terms.push(term);
         }
-
-        let items = line
-            .rsplit(&['[', ']'])
-            .collect::<Vec<_>>();
-        if items.len() <= 2 || items[items.len() - 1].trim() != "-"
-        {
-            continue;
-        }
-
-        let name = items[1].to_string();
-        let mut link = items[0].replace(['(', ')'], "");
-        let mut content = String::new();
-
-        //  Get content
-        if let Some((link_part, title)) = link.clone().split_once('#')
-        {
-            let link_path = filepath.join(Path::new(link_part));
-            link = link_path.to_str().unwrap().to_string();
-            if link_path.exists() && link_path.is_file()
-            {
-                let target = "# ".to_string() + &title.to_lowercase();
-                let input = fs::read_to_string(link_path).unwrap();
-                if let Some(start) = input.find(&target)
-                {
-                    content = input[(start + target.len())..].to_string();
-                    if let Some(end) = content.find("# ")
-                    {
-                        content = content[..(end - 2)].to_string();
-                    }
-                }
-            }
-        }
-
-        let term = Term
-        {
-            name,
-            link: link.to_string(),
-            content: markdown_to_text::convert(content.trim()),
-        };
-        result.push(term);
     }
 
-    result
+    terms
 }
 
-//  Output report.
-fn report( output_path: PathBuf, note_path: PathBuf, terms: &[Term] )
+//  Parses a line of checksheet and convert it to a term.
+fn parse_checksheet_line( line: &str ) -> Option<Term>
 {
-    let report_file_path = get_file_path(output_path, "report");
-    let mut report_file = File::create(report_file_path)
-        .expect("failed to create a report file");
+    //  Gets the part enclosed by `[]` and `()` from the line.
+    let parts = line
+        .split(|c| c == '[' || c == ']' || c == '(' || c == ')')
+        .map(|s| s.trim())
+        .filter(|s|
+            s.is_empty() == false
+            && s != &"-"
+            && s.starts_with(&"#") == false
+        )
+        .collect::<Vec<&str>>();
 
-    let mut note_path = note_path.clone();
-    note_path.push("**/*.md");
-    let files = glob(&note_path.into_os_string().into_string().unwrap())
-        .unwrap()
-        .map(|e| e.unwrap())
-        .collect::<Vec<_>>();
-
-    //  Check for broken links
-    let link_regex = regex::Regex::new(r"\[.*?\]\(([^)\s]+)\)").unwrap();
-    for file in files
+    //  If line contains a term, it should be of the form `[term, link]`.
+    if parts.len() != 2
     {
-        let content = fs::read_to_string(file.clone()).unwrap();
-
-        for (n, line) in content.lines().enumerate()
-        {
-            for capture in link_regex.captures_iter(line)
-            {
-                if let Some(link) = capture.get(1)
-                {
-                    let link = link.as_str();
-                    if link.starts_with('#')
-                    {
-                        continue;
-                    }
-
-                    if check_link(link, file.clone()) == false
-                    {
-                        let log = format!
-                        (
-                            "[{}:{}] link error: {}",
-                            file.as_os_str().to_str().unwrap(),
-                            n + 1,
-                            link
-                        );
-                        println!("{}", log);
-                        let _ = writeln!(report_file, "{}", log);
-                    }
-                }
-            }
-        }
+        return None;
     }
 
-    //  Check if sentences can be a quiz.
-    for term in terms
+    let term = parts[0];
+    let (link, flagment) = if parts[1].contains('#')
     {
-        let name = &term.name;
-        let content = &term.content;
-        if content.contains(name) == false
-        {
-           let log = format!
-            (
-                "the term '{}' is not contained in the '{}'",
-                name,
-                content,
-            );
-            println!("{}", log);
-            let _ = writeln!(report_file, "{}", log);
-        }
-    }
-}
-
-fn check_link( link: &str, filepath: PathBuf ) -> bool
-{
-    if link.starts_with("http")
-    {
-        let response = reqwest::blocking::get(link);
-        if let Ok(res) = response
-        {
-            return res.status().is_success();
-        }
-    }
-
-    let base_path = filepath.parent().unwrap();
-    if let Some((link, title)) = link.split_once('#')
-    {
-        let link_path = base_path.join(Path::new(link));
-        if link_path.exists() && link_path.is_file()
-        {
-            let content = fs::read_to_string(link_path).unwrap();
-            let target = "# ".to_string() + &title.to_lowercase();
-            return content.to_lowercase().contains(&target);
-        }
+        //  If the link contains a flagment, splits it.
+        let mut parts = parts[1].split_once('#').unwrap();
+        (convert_path(parts.0), parts.1)
     }
     else
     {
-        let link_path = base_path.join(Path::new(link));
-        return link_path.exists();
-    }
+        (convert_path(parts[1]), "")
+    };
 
-    return false;
+    //  Extracts a specific fragment from the note.
+    let content = fs::read_to_string(link).unwrap_or("".to_string());
+    let content = if flagment.is_empty()
+    {
+        content
+    }
+    else
+    {
+        //  Splits sections by lines startnig with `#`.
+        let mut sections = Vec::new();
+        let mut start = 0;
+        let mut is_in_section = false;
+        for (index, line) in content.lines().enumerate()
+        {
+            if line.starts_with("#")
+            {
+                if is_in_section
+                {
+                    let section = &content[start..index];
+                    sections.push(section.trim());
+                    is_in_section = false;
+                }
+
+                start = index;
+                is_in_section = true;
+            }
+        }
+        println!("{:?}", sections);
+
+        "".to_string()
+    };
+
+    Some(Term
+    {
+        term: String::new(),
+        link: String::new(),
+        flagment: String::new(),
+        content: String::new(),
+    })
+}
+
+//------------------------------------------------------------------------------
+//  Converts path to path from base path.
+//------------------------------------------------------------------------------
+fn convert_path( path: &str ) -> String
+{
+    let cli = Cli::parse();
+    let mut note_path = PathBuf::from(cli.note_path.clone());
+    note_path.push(path);
+    note_path.into_os_string().into_string().unwrap_or("".to_string())
 }
