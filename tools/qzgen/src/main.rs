db6b5d00539d0cb786e8cf52cb8b0719716965cb
ichigo <dev.honda.ichigo@gmail.com>
2023-06-24 20:32:36 +0900

    tools: Upate QZgen

--- tools/qzgen/src/main.rs
+++ tools/qzgen/src/main.rs
@@ -1,17 +1,23 @@
 /*
 
-    A CLI tool that automatically generates quizes from knowledge notes.
+    A GUI tool that automatically generates quizes from knowledge notes.
 
 */
 
 use std::path::{ Path, PathBuf };
 use std::fs::{ self, File };
 use std::io::Write;
+use std::fmt;
 
 use clap::Parser;
+use chrono::Local;
+use markdown::{ self, Block, Span, ListItem };
+use once_cell::sync::OnceCell;
+use rand::seq::SliceRandom;
+use regex::Regex;
 use serde::{ Serialize, Deserialize };
 use serde_json;
-use rand::seq::SliceRandom;
+use termion::{ color, style };
 
 #[derive(Parser)]
 #[clap(author, version, about)]
@@ -48,9 +54,22 @@ struct Cli
     output_path: String,
 }
 
+//  Static log file.
+static LOG_FILE: OnceCell<File> = OnceCell::new();
+
 fn main()
 {
     let cli = Cli::parse();
+
+    //  Creates a log file.
+    let mut output_path = PathBuf::from(cli.output_path);
+    output_path.push
+    (
+        format!("learning_{}.log", Local::now().format("%Y%m%d%H%M%S"))
+    );
+    let log_file = File::create(output_path)
+        .expect("failed to create log file");
+    LOG_FILE.set(log_file).unwrap();
 
     //  Parses the checksheet and convert it to a vector of terms.
     let mut terms = if Path::new("terms.cache").is_file()
@@ -70,18 +89,50 @@ fn main()
     terms.shuffle(&mut rand::thread_rng());
 
     //  Generates a quiz for each term.
-    for (index, term) in terms.enumerate()
+    let mut result = Result
     {
-        println!("Q-{}", index + 1);
-        quiz(term);
+        number_of_quiz: 0,
+        correct: 0,
+        incorrect_terms: Vec::new(),
+    };
+    for term in terms
+    {
+        result.number_of_quiz += 1;
+        output!("Q-{}\n\n", result.number_of_quiz);
+        quiz(&term, &mut result);
+        output!("\n\n");
     }
 }
 
+//------------------------------------------------------------------------------
+//  A structure 
+//------------------------------------------------------------------------------
+#[derive(Debug)]
+struct Result
+{
+    number_of_quiz: usize,
+    correct: usize,
+    incorrect_terms: Vec<Term>,
+}
+
+impl fmt::Display for Result
+{
+    fn fmt( &self, f: &mut fmt::Formatter<'_> ) -> fmt::Result
+    {
+        write!
+        (
+            f,
+            "Result: {}/{} ({}%)",
+            self.correct, self.number_of_quiz,
+            self.correct as f32 / self.number_of_quiz as f32 * 100.0
+        )
+    }
+}
 
 //------------------------------------------------------------------------------
 //  A structure representing a term.
 //------------------------------------------------------------------------------
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Term
 {
     //  Term.
@@ -172,8 +223,7 @@ fn parse_checksheet_line( line: &str ) -> Option<Term>
                 {
                     break;
                 }
-
-                section.push_str(line);
+                section.push_str(&(line.to_owned() + "\n"));
             }
             else if line.to_lowercase().contains(&("# ".to_string() + flagment))
             {
@@ -224,9 +274,317 @@ fn load_terms_cache() -> Vec<Term>
 }
 
 //------------------------------------------------------------------------------
-//  Generates a quiz for a term.
+//  Generates a quiz for a term. Returns true if the answer is correct.
 //------------------------------------------------------------------------------
-fn quiz( term: Term )
+fn quiz( term: &Term, result: &mut Result )
 {
-    println!("{}");
+    let quiz = generate_quiz(term);
+    print_quiz(quiz);
+
+    let max_retry_cnt = 3;
+    let mut retry_cnt = 0;
+    let mut hint_cnt = 0;
+    loop
+    {
+        output!("Please input answer. [h(hint)/s(skip)/q(quit)] >> ");
+        std::io::stdout().flush().unwrap();
+        match get_input()
+        {
+            s if s == "h".to_string() =>
+            {
+                hint_cnt += 1;
+                if hint_cnt == term.term.chars().count()
+                {
+                    return;
+                }
+
+                for (i, char) in term.term.chars().enumerate()
+                {
+                    if i < hint_cnt
+                    {
+                        output!("{}", char);
+                    }
+                    else
+                    {
+                        output!("_");
+                    }
+                }
+                output!("\n");
+            },
+            s if s == "s".to_string() =>
+            {
+                outputln!("Skip this quiz.");
+                outputln!("Answer: {}", term.term);
+                result.number_of_quiz -= 1;
+                return;
+            },
+            s if s == "q".to_string() =>
+            {
+                result.number_of_quiz -= 1;
+                quit_quiz(result);
+            },
+            s =>
+            {
+                let lower_s = s.trim().to_lowercase();
+                let lower_term = term.term.trim().to_lowercase();
+                if lower_s == lower_term
+                    || lower_s.len() >= 5 && lower_term.contains(&lower_s)
+                {
+                    outputln!("Correct!");
+                    outputln!("Answer: {}", term.term);
+                    result.correct += 1;
+                    return;
+                }
+                else
+                {
+                    retry_cnt += 1;
+                    if retry_cnt >= max_retry_cnt
+                    {
+                        outputln!("Failed...");
+                        outputln!("Answer: {}", term.term);
+                        result.incorrect_terms.push(term.clone());
+                        return;
+                    }
+
+                    outputln!("Retry({}/{})", retry_cnt, max_retry_cnt);
+                }
+            },
+        }
+    }
+}
+
+//  Generates a quiz for a term.
+fn generate_quiz( term: &Term ) -> String
+{
+    let mut content = term.content.clone();
+    let target = &term.term;
+    let replace = r" [xxxxx] ";
+
+    //  Masks the term and alias.
+    content = content.replace(target, replace);
+    let re = Regex::new(r"\(([^\)]*)\)|（([^）]*)）").unwrap();
+    for cap in re.captures_iter(&target)
+    {
+        let mut alias = String::new();
+        if let Some(s) = cap.get(1)
+        {
+            alias = s.as_str().to_string();
+        }
+        else if let Some(s) = cap.get(2)
+        {
+            alias = s.as_str().to_string();
+        }
+
+        if alias.is_empty() == false
+        {
+            content = content.replace(&alias, replace);
+            break;
+        }
+    }
+
+    content.trim().to_string()
+}
+
+//  Prints a quiz.
+fn print_quiz( quiz: String )
+{
+    let tokens = markdown::tokenize(&quiz);
+    for token in tokens
+    {
+        print_block(token);
+    }
+    output!("\n\n");
+}
+
+fn print_block( block: Block )
+{
+    match block
+    {
+        Block::Header(v, _) =>
+        {
+            output!("{}", color::Bg(color::Cyan));
+            for span in v
+            {
+                print_span(span);
+            }
+            output!("{}", style::Reset);
+        },
+        Block::Paragraph(v) =>
+        {
+            for span in v
+            {
+                print_span(span);
+            }
+        },
+        Block::Blockquote(v) =>
+        {
+            for block in v
+            {
+                print_block(block);
+            }
+        },
+        Block::CodeBlock(_, s) =>
+        {
+            outputln!
+            (
+                "\n=====\n{}{}{}\n=====\n",
+                color::Bg(color::LightBlack),
+                s,
+                style::Reset
+            );
+        },
+        Block::OrderedList(v, _) =>
+        {
+            for (index, item) in v.into_iter().enumerate()
+            {
+                output!("\n{}. ", index + 1);
+                print_item(item);
+            }
+            output!("\n");
+        },
+        Block::UnorderedList(v) =>
+        {
+            for item in v
+            {
+                output!("\n* ");
+                print_item(item);
+            }
+            output!("\n");
+        },
+        Block::Raw(s) =>
+        {
+            outputln!("{}", s);
+        },
+        Block::Hr =>
+        {
+            outputln!("{}", std::iter::repeat("-").take(80).collect::<String>());
+        },
+    }
+}
+
+fn print_span( span: Span )
+{
+    match span
+    {
+        Span::Break => { output!("\n"); },
+        Span::Text(s) =>
+        {
+            if s.starts_with('|')
+            {
+                output!("\n{}", s);
+            }
+            else
+            {
+                output!("{}", s);
+            }
+        }
+        Span::Code(s) =>
+        {
+            output!("{}{}{}", color::Bg(color::LightBlack), s, style::Reset);
+        },
+        Span::Link(s, _, _) =>
+        {
+            output!
+            (
+                "{}{}{}{}",
+                color::Fg(color::Blue),
+                style::Underline,
+                s,
+                style::Reset
+            );
+        }
+        Span::Emphasis(v) =>
+        {
+            output!("{}", style::Italic);
+            for span in v
+            {
+                print_span(span);
+            }
+            output!("{}", style::Reset);
+        },
+        Span::Strong(v) =>
+        {
+            output!("{}", style::Bold);
+            for span in v
+            {
+                print_span(span);
+            }
+            output!("{}", style::Reset);
+        },
+        _ => {},
+    }
+}
+
+fn print_item( item: ListItem )
+{
+    match item
+    {
+        ListItem::Simple(v) =>
+        {
+            for span in v
+            {
+                print_span(span);
+            }
+        },
+        ListItem::Paragraph(v) =>
+        {
+            for block in v
+            {
+                print_block(block);
+            }
+        },
+    }
+}
+
+//------------------------------------------------------------------------------
+//  Gets user input.
+//------------------------------------------------------------------------------
+fn get_input() -> String
+{
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().to_string()
+}
+
+//------------------------------------------------------------------------------
+//  Quits the quiz.
+//------------------------------------------------------------------------------
+fn quit_quiz( result: &Result )
+{
+    if result.number_of_quiz > 0
+    {
+        outputln!("{}", result);
+    }
+    std::process::exit(0);
+}
+
+//------------------------------------------------------------------------------
+//  Macro for output message.
+//------------------------------------------------------------------------------
+#[macro_export]
+macro_rules! output
+{
+    ($($arg:tt)*) =>
+    {
+        let mut s = String::new();
+        std::fmt::write(&mut s, format_args!($($arg)*)).unwrap();
+        print!("{}", s);
+
+        let mut log_file = LOG_FILE.get().unwrap();
+        let _ = write!(log_file, "{}", s);
+    };
+}
+
+#[macro_export]
+macro_rules! outputln
+{
+    ($($arg:tt)*) =>
+    {
+        let mut s = String::new();
+        std::fmt::write(&mut s, format_args!($($arg)*)).unwrap();
+        println!("{}", s);
+
+        let mut log_file = LOG_FILE.get().unwrap();
+        let _ = writeln!(log_file, "{}", s);
+    };
 }
