60d4356cd9793307b482863bf0d35c548c3201f2
ichigo <dev.honda.ichigo@gmail.com>
2023-07-07 09:16:05 +0900

    tools: Update QZgen

--- tools/qzgen/src-tauri/src/data.rs
+++ tools/qzgen/src-tauri/src/data.rs
@@ -11,4 +11,13 @@ pub struct Term
 {
     term_id: i64,
     term: String,
+    content: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Mask
+{
+    mask_id: i64,
+    term_id: i64,
+    mask: String,
 }
