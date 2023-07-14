2a5ce7f148b81c9511a284b636be3e0111d45b90
ichigo <dev.honda.ichigo@gmail.com>
2023-07-14 11:40:59 +0900

    tools: Update lambda function

--- tools/lambda_functions/generate_random_quiz/lambda_function.py
+++ tools/lambda_functions/generate_random_quiz/lambda_function.py
@@ -1,5 +1,3 @@
-import sys
-import os
 import mysql.connector
 import json
  
@@ -9,22 +7,28 @@ DB_PORT = 3306
 DB_PASSWORD = "Y77rZ21mHyMG9tagC^n7"
 DB_HOST = "ichigo-dev-knowledge-db.c5ecesxssz8k.ap-northeast-1.rds.amazonaws.com"
 DB_NAME = "qzgen"
- 
+
 def lambda_handler(event, context):
     try:
-        conn = mysql.connector.connect(host=DB_HOST, port=DB_PORT, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
+        conn = mysql.connector.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME)
     except Exception  as e:
         print("Fail connecting to RDS mysql instance")
         print(e)
         sys.exit()
  
-    result = []
-    with conn.cursor() as cur:
-        cur.execute("SELECT * FROM `term` ORDER BY rand() LIMIT 1")
-        for row in cur:
-            result.append(row)
- 
+    cursor = conn.cursor(dictionary=True)
+    sql = "SELECT * FROM `term` ORDER BY rand() LIMIT 1"
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    cursor.close()
+    conn.close()
+
     return {
         'statusCode': 200,
-        'body': json.dumps(result, indent=2)
+        'body': json.dumps(result, indent=2, default=str)
     }
